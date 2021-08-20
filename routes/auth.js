831078fc2f2c987754763c6ee01883698fe339c5
Yannik195 <yannisim@gmail.com>
2021-08-20 16:52:45 +0200

    check db for matching user

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const User = require("../models/User")
 router.post("/login", async (req, res) => {
     console.log(req.body)
     try {
-        const user = await User.find()
+        const user = await User.find({ email: req.body.email })
         res.json(user)
     } catch (err) {
         res.json({ message: err })
