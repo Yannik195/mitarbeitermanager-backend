fdbb1124787e894111afc2b3e87d53467ee5053c
Yannik195 <yannisim@gmail.com>
2021-08-24 11:46:18 +0200

    pw and email

--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,38 @@ const User = require("../models/User")
 const jwt = require("jsonwebtoken");
 
 router.post("/login", async (req, res) => {
-
+    // Todo
     try {
-        const user = await User.findOne({ email: req.body.email }).exec()
-        res.json(user)
+        // query user in db
+        const user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            // no user found with this mail
+            console.log("no user found")
+            res.status(401).send("no user found")
+        } else {
+            // user found 
+            // check for matching pw
+            if (req.body.password == user.password) {
+                // pw and mail match
+                res.json(user)
+            } else {
+                // wrong pw
+                res.status(401).send("wrong password")
+            }
+        }
+
     } catch (err) {
-        res.status(401)
-        res.send("Wrong Email!")
+        res.send(err)
     }
 
-    const public = req.body.email + req.body.password
-    const accessToken = jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 Hour expiry
-        data: public
-    }, process.env.ACCESS_TOKEN_SECRET);
-    console.log(accessToken)
-    res.json({ accessToken: accessToken })
+    // const public = req.body.email + req.body.password
+    // const accessToken = jwt.sign({
+    //     exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 Hour expiry
+    //     data: public
+    // }, process.env.ACCESS_TOKEN_SECRET);
+    // console.log(accessToken)
+    // res.json({ accessToken: accessToken })
 })
 
 router.post("/signup", async (req, res) => {
