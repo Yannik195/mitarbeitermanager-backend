598338d549fdf8285957024b30a74bc2234c6992
Yannik195 <yannisim@gmail.com>
2021-08-24 09:59:24 +0200

    echte date von db abfragen

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 const express = require("express")
 const router = express.Router();
 const User = require("../models/User")
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 router.post("/login", async (req, res) => {
-    const accessToken = jwt.sign(req.body.email, process.env.ACCESS_TOKEN_SECRET)
+
+    try {
+        const user = await User.findOne({ email: req.body.email }).exec()
+        res.json(user)
+    } catch (err) {
+        res.status(401)
+        res.send("Wrong Email!")
+    }
+
+    const public = req.body.email + req.body.password
+    const accessToken = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 Hour expiry
+        data: public
+    }, process.env.ACCESS_TOKEN_SECRET);
     console.log(accessToken)
     res.json({ accessToken: accessToken })
 })
