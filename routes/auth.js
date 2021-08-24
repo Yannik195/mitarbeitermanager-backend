6a2bb92771f6acbbd5b2988839d2b64f838693dd
Yannik195 <yannisim@gmail.com>
2021-08-24 20:08:45 +0200

    login auth etc

--- routes/auth.js
+++ routes/auth.js
@@ -36,22 +36,50 @@ router.post("/login", async (req, res) => {
     } catch (err) {
         res.send(err)
     }
-
-
 })
+
+let sendToken = (email, password) => {
+    const public = email + password
+    const accessToken = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 Hour expiry
+        data: public
+    }, process.env.ACCESS_TOKEN_SECRET);
+    console.log(accessToken)
+    res.json({ accessToken: accessToken })
+}
 
 router.post("/signup", async (req, res) => {
     console.log(req.body)
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-    try {
-        const savedUser = await user.save()
-        res.json(savedUser)
-    } catch (err) {
 
+    // check if user exists already
+    const user = await User.findOne({ email: req.body.email })
+    if (user) {
+        // user exists already
+        console.log("user exists already")
+        res.status(401)
+    } else {
+        // user doesnt exist, create new one
+        console.log("create new user")
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password
+        })
+        try {
+            const savedUser = await user.save()
+            console.log(savedUser)
+            //Send token so user can login
+            const public = savedUser.email + savedUser.password
+            const accessToken = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 Hour expiry
+                data: public
+            }, process.env.ACCESS_TOKEN_SECRET);
+            console.log(accessToken)
+            res.json({ accessToken: accessToken })
+        } catch (err) {
+
+        }
     }
+
 })
 
 
